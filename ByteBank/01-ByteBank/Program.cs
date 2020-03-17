0a9be322f666335d1441a36270f3e64d97ae1826
Vini Black <viniibl4ck@gmail.com>
2020-03-16 22:20:36 -0300

    Adicionando as informações da conta da Gabriela

--- ByteBank/01-ByteBank/Program.cs
+++ ByteBank/01-ByteBank/Program.cs
@@ -10,8 +10,21 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaGabriela = new ContaCorrente();
 
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 863;
+            contaGabriela.numero = 863452;
+            contaGabriela.saldo = 100;
+
+            Console.WriteLine(contaGabriela.titular);
+            Console.WriteLine("Agência: " + contaGabriela.agencia);
+            Console.WriteLine("Número: " + contaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaGabriela.saldo);
+
+
+            contaGabriela.saldo += 200;
+            Console.WriteLine("Saldo: " + contaGabriela.saldo);
             Console.ReadLine();
 
         }
