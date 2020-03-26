53bf3497e5a32c0d0360d6fbdba340ccac105bc5
Black <viniibl4ck@gmail.com>
2020-03-26 18:50:38 -0300

    Entendendo como funciona o private, e criando ObterSaldo e o DefinirSaldo para não conseguir passar um numero negativo no Program.cs

---
+++ ByteBank/06 - ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using _06_ByteBank;
+using System;
+
+namespace _06___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.DefinirSaldo(10);
+
+            Console.WriteLine(conta.ObterSaldo());
+
+            Console.ReadLine();
+        }
+    }
+}
