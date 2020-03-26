77de0db9896a2e0a8bd36c69337f10015b16a6f5
Black <viniibl4ck@gmail.com>
2020-03-26 19:27:05 -0300

    Deixando o codigo menos verboso, usando o Get e Set no Titular

--- ByteBank/06 - ByteBank/Program.cs
+++ ByteBank/06 - ByteBank/Program.cs
@@ -8,10 +8,17 @@ namespace _06___ByteBank
         static void Main(string[] args)
         {
             ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
 
-            conta.DefinirSaldo(10);
+            cliente.nome = "Guilherme";
+            cliente.cpf = "434.564.879-20";
+            cliente.profissao = "Desenvolvedor";
 
-            Console.WriteLine(conta.ObterSaldo());
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Saldo);
 
             Console.ReadLine();
         }
