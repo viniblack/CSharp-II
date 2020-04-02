9902bf114a2f76adf3e8dcdeaa5c3f9d07137831
Black <viniibl4ck@gmail.com>
2020-04-02 15:03:21 -0300

    Criando um construtor que faz a agencia e o numero da conta serem obrigadorias

---
+++ ByteBank/07 - ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
